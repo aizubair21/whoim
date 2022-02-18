793488e55c586e1571e951d7fc56811683c9776e
aizubair21 <amimulit21@gmail.com>
2022-02-18 10:27:19 +0600

    test

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
-
+/*progress bar plugins*/
+/*skill slider object*/
 var object = [
     {
         "headline":"HTML & CSS",
@@ -21,13 +22,14 @@ var object = [
 
 ];
 
+/*owl-carousel slider banner slider bar*/
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
-        center: true,
+        center:true,
         loop:true,
         margin:10,
         nav:true,
-        items:4,
+        items:5,
         Responsive:{
             600: {
                 items:3,
@@ -38,9 +40,12 @@ $(document).ready(function(){
             768: {
                 items:4,
             }
-        }
+        },
         
     });
+    
+    /*testimonial skill slider*/
+
     $("#skill_set").skillset({
         object:object,
         duration:100,
@@ -53,40 +58,44 @@ $(document).ready(function(){
         $("#nav_link").css({"left":"-100%"});
     })
 
-    $("#owl-carousel-testimonial").owlCarousel({
-        loop:true,
-        center:true,
-        margin:10,
-        nav:true,
-        items:1,
-    })
-  });
+   
+});
 
-  var slideIndex = 1;
-  showSlides(slideIndex);
-  
-  // Next/previous controls
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  
-  // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-  
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-  }
+/*owl-carousel blog section slider*/
+$("#blog .owl-carousel").owlCarousel({
+    loop:true,
+    margin:10,
+    items:3,
+    
+});
+
+
+/*testimonial slider*/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+var i;
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("dot");
+if (n > slides.length) {slideIndex = 1}
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+}
+for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " active";
+}
